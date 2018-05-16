173499c204875d707605f17530447be49e0fb814
Raphael Thomazella <1357rtr@gmail.com>
2018-05-15 23:40:10 -0300

    added comments, gitignore for my .vscode, some questions.txt

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ export styled, {
   ServerStyleSheet,
   StyleSheetManager
 } from "styled-components";
+/*
+I find it super confusing how you export all those above from styled-components then alias this folder to 'reas.'
+Looks like reas implements those above but in reality it just exports from styled-components.
+
+It's also confusing as hell as many files are called 'index.js'
+*/
+
 export * from "./components";
 
 export { as };
